d40f2cc2d2d257d84ff13de50be084dcbf95eb4d
Lindrit Krasniqi <linndritks@gmail.com>
2021-11-29 12:55:58 +0100

    Create simple test

---
+++ tests/Feature/SimpleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace lindritkrasniqi\Larapack\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+/**
+ * SimpleTest::class
+ */
+class SimpleTest extends TestCase
+{
+    /** @test */
+    public function is_it_true_really_true()
+    {
+        $this->assertTrue(true);
+    }
+}
